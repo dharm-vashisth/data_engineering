c14db1b260a5d2c354c5dd3d5a3ad12a2289bc0c
Dharm Vashisth <dharmvashisth5@gmail.com>
2026-04-11 23:09:29 +0530

    lazy evaluation for optimization.

---
+++ my_first_pipeline/models/dataframe_from_csv_using_polars_lazy_evaluation.py
@@ -0,0 +1,30 @@
+import polars as pl
+import os
+from constants import root
+from logger import get_logging_loader
+
+file_path = os.path.join(root,"data_inputs","raw_data.csv")
+log_file_path = os.path.join(root,"logs","lazy_evaluation.log")
+lazy_logger = get_logging_loader(logger_name="lazy_logger",file_name=log_file_path)
+
+if __name__ == "__main__":
+    # lazy evaluation
+    data = pl.scan_csv(file_path)
+
+    # plan
+    df = (
+        data.with_columns(
+            pl.col("Salary").cast(pl.Int64,strict=False)
+        ).filter(pl.col("Salary")>44)
+    )
+
+    # explain the plan
+    print("The lazy execution plan:")
+    print(df.explain())
+
+    # execute the plan
+    print("The plan is going to execute...")
+    filtered_df = df.collect()
+
+    print("plan is executed successfully!")
+    print(filtered_df)
