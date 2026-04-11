c3051d6c8cf54576a0780baa4f49e66cfc53032e
Dharm Vashisth <dharmvashisth5@gmail.com>
2026-04-11 22:10:43 +0530

    counting of null records in a column and type casting.

--- my_first_pipeline/models/dataframe_from_csv_using_polars.py
+++ my_first_pipeline/models/dataframe_from_csv_using_polars.py
@@ -1,14 +1,30 @@
 import polars as pl
 from constants import root
 import os
+from logger import get_logging_loader
+
+bad_data_logger = get_logging_loader(logger_name="bad_data_logger",file_name="bad_data_logger.log")
 
 if __name__=="__main__":
     file_path = os.path.join(root,"data_inputs","raw_data.csv")
-    df = pl.read_csv(file_path, has_header=False)
+    df = pl.read_csv(file_path, has_header=True)
     print("Original Dataframe:")
     df.show()
-    null_df = df.filter(pl.col("column_3").is_not_null())
-    filtered_df = df.filter(pl.col("column_3").is_not_null())
-    print("Filtered Dataframe:")
+    # type casting
+    df = df.with_columns(
+        pl.col("Salary").cast(pl.Int64)
+    )
+    
+    # total records.
+    total_records_salaries = df.select(pl.col("Salary").len()).item()
+    # null salary records.
+    null_salaries_df = df.select(pl.col("Salary").null_count()).item()
+    null_salary_percent = (null_salaries_df/total_records_salaries)*100
+
+    if null_salary_percent > 10:
+        bad_data_logger.info(f"Null Salary records are greater than 10% {null_salary_percent}")
+    
+    filtered_df = df.filter(pl.col("Salary").is_not_null())
+    print("Filtered Dataframe with non-null salaries:")
     filtered_df.show()
     
